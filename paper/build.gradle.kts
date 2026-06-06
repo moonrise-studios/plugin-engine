53b84956f81bc710c9ed48297c21dcd3da55884f
Eric <60104846+ericlmao@users.noreply.github.com>
2026-06-05 22:10:43 -0300

    Bump Paper API version to 1.2.4
    Update the apiVersion variable in paper/build.gradle.kts from 1.2.3 to 1.2.4 to track the new API revision.

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 var id = "plugin-engine-paper"
 var domain = "gg.moonrise.engine"
-var apiVersion = "1.2.3"
+var apiVersion = "1.2.4"
 
 repositories {
     maven("https://repo1.maven.org/maven2/")
